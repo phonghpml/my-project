e42a7ed8c00178b09ae5af0aa68cc55508feb2c7
Phong Bùi <buitrongphonghp@gmail.com>
2023-09-09 16:33:43 +0700

    update code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
